1f273bfd38244e11b732dee0a82fc1e6c06e0e84
BraveVlad <giladvladpinker@gmail.com>
2023-11-28 15:12:36 +0200

    GridView shows products list

--- dist/main.js
+++ dist/main.js
@@ -1,8 +1,17 @@
 import * as Product from "./Product.model.js";
+var gridView = document.querySelector(".products-grid");
 function onProductsLoaded(products) {
     console.log("products loaded, can continue");
     console.log(products);
     // continue app 
+    // TODO - update global products array with loaded products
+    renderProductsGridView(products);
+}
+function renderProductsGridView(products) {
+    gridView.innerHTML = "\n        " + products.map(renderGridItem) + "\n    ";
+}
+function renderGridItem(product) {
+    return "\n    <li class=\"products-grid__item\">\n        <div class=\"product-view\" data-product-id=\"" + product.PID + "\">\n            <img src=\"" + product.pic_path + "\" alt=\"\" class=\"product-view__img\" width=\"100\" height=\"100\">\n            <h4 class=\"product-view__title\">" + product.name + "</h4>\n            <h6 class=\"product-view__price\">" + product.price + "</h6>\n            <button class=\"product-view__add-button\">add to cart</button>\n        </div>\n    </li>\n    ";
 }
 function main() {
     console.log("Loading products from file...");
--- main.ts
+++ main.ts
@@ -2,12 +2,36 @@
 import * as Product from "./Product.model.js";
 
 
+const gridView = document.querySelector(".products-grid") as HTMLUListElement;
+
+
 function onProductsLoaded(products: Product.Product[]) {
     console.log("products loaded, can continue")
     console.log(products)
 
     // continue app 
+    // TODO - update global products array with loaded products
+    renderProductsGridView(products);
+}
 
+function renderProductsGridView(products: Product.Product[]) {
+
+    gridView.innerHTML = `
+        ${products.map(renderGridItem)}
+    `;
+}
+
+function renderGridItem(product: Product.Product) {
+    return `
+    <li class="products-grid__item">
+        <div class="product-view" data-product-id="${product.PID}">
+            <img src="${product.pic_path}" alt="" class="product-view__img" width="100" height="100">
+            <h4 class="product-view__title">${product.name}</h4>
+            <h6 class="product-view__price">${product.price}</h6>
+            <button class="product-view__add-button">add to cart</button>
+        </div>
+    </li>
+    `;
 }
 
 function main() {
@@ -17,6 +41,5 @@ function main() {
 
 }
 
-
-
 main();
+
