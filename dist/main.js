b97dce866b10be69dc83e2c114f4b708661f42ad
BraveVlad <giladvladpinker@gmail.com>
2023-11-28 15:42:21 +0200

    Grid items fire "Add To Cart" event.

--- dist/main.js
+++ dist/main.js
@@ -8,10 +8,30 @@ function onProductsLoaded(products) {
     renderProductsGridView(products);
 }
 function renderProductsGridView(products) {
-    gridView.innerHTML = "\n        " + products.map(renderGridItem) + "\n    ";
+    gridView.innerHTML = "\n        " + products.map(renderGridItem).join("\n") + "\n    ";
+    var addButtons = gridView.querySelectorAll(".product-view__add-button");
+    attachAddToCartEvent(addButtons);
 }
 function renderGridItem(product) {
-    return "\n    <li class=\"products-grid__item\">\n        <div class=\"product-view\" data-product-id=\"" + product.PID + "\">\n            <img src=\"" + product.pic_path + "\" alt=\"\" class=\"product-view__img\" width=\"100\" height=\"100\">\n            <h4 class=\"product-view__title\">" + product.name + "</h4>\n            <h6 class=\"product-view__price\">" + product.price + "</h6>\n            <button class=\"product-view__add-button\">add to cart</button>\n        </div>\n    </li>\n    ";
+    return "\n    <li class=\"products-grid__item\">\n        <div class=\"product-view\" >\n            <img src=\"" + product.pic_path + "\" alt=\"\" class=\"product-view__img\" width=\"100\" height=\"100\">\n            <h4 class=\"product-view__title\">" + product.name + "</h4>\n            <h6 class=\"product-view__price\">" + product.price + "</h6>\n            <button class=\"product-view__add-button\" data-product-id=\"" + product.PID + "\">add to cart</button>\n        </div>\n    </li>\n    ";
+}
+// controller
+function attachAddToCartEvent(buttons) {
+    buttons.forEach(function (buttons) { return buttons.addEventListener("click", function (event) {
+        var target = event.target;
+        var productId = target.getAttribute("data-product-id");
+        if (!productId)
+            throw new Error("Add to cart event - Couldn't find product ID to add to cart.");
+        var convertedId = Number(productId);
+        if (Number.isNaN(convertedId))
+            throw new Error("Add to cart event - Product ID isnt a number");
+        onAddToCartEvent(convertedId);
+    }); });
+}
+// contoller
+function onAddToCartEvent(productId) {
+    console.log(productId);
+    var product = Product.getProductbyID(productId);
 }
 function main() {
     console.log("Loading products from file...");
--- main.ts
+++ main.ts
@@ -17,22 +17,87 @@ function onProductsLoaded(products: Product.Product[]) {
 function renderProductsGridView(products: Product.Product[]) {
 
     gridView.innerHTML = `
-        ${products.map(renderGridItem)}
+        ${products.map(renderGridItem).join("\n")}
     `;
+
+    const addButtons = gridView.querySelectorAll(".product-view__add-button") as NodeListOf<HTMLButtonElement>;
+    attachAddToCartEvent(addButtons);
 }
 
 function renderGridItem(product: Product.Product) {
     return `
     <li class="products-grid__item">
-        <div class="product-view" data-product-id="${product.PID}">
+        <div class="product-view" >
             <img src="${product.pic_path}" alt="" class="product-view__img" width="100" height="100">
             <h4 class="product-view__title">${product.name}</h4>
             <h6 class="product-view__price">${product.price}</h6>
-            <button class="product-view__add-button">add to cart</button>
+            <button class="product-view__add-button" data-product-id="${product.PID}">add to cart</button>
         </div>
     </li>
     `;
 }
+
+// controller
+function attachAddToCartEvent(buttons: NodeListOf<HTMLButtonElement>) {
+    buttons.forEach(buttons => buttons.addEventListener("click", (event) => {
+        const target = event.target as HTMLButtonElement;
+        const productId = target.getAttribute("data-product-id") as string;
+
+        if (!productId) throw new Error("Add to cart event - Couldn't find product ID to add to cart.");
+
+        const convertedId = Number(productId);
+        if (Number.isNaN(convertedId)) throw new Error("Add to cart event - Product ID isnt a number")
+
+        onAddToCartEvent(convertedId);
+    }));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// contoller
+function onAddToCartEvent(productId: number) {
+
+    console.log(productId);
+
+    const product = Product.getProductbyID(productId);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function main() {
 
